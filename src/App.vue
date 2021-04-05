1ca35dbb8c010ab2f84769215723f0b923d86192
calumpetergunn <calumgunn@gmail.com>
2021-04-05 16:06:01 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="task, index in tasks" v-bind:key="index">
+        <span v-bind:class="task.priority === 'high' ? 'high': 'low'">{{task.name}}</span>
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="saveNewTask">
+      <label for="new-task">Add a New Task:</label>
+      <input type="text" id="new-task" v-model="newTask" required>
+      <input type="radio" id="high-priority" name="new-priority" value="high" v-model="newPriority" required>
+      <label for="high-priority">High Priority</label>
+      <input type="radio" id="low-priority" name="new-priority" value="low" v-model="newPriority">
+      <label for="low-priority">Low Priority</label>
+      <input type="submit" value="Add Task">
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      tasks: [
+        {name:"Vacuum", priority:"high" },
+        {name:"Feed Hamster", priority: "low" },
+        {name:"Wash Car", priority: "low" }
+      ],
+      newTask: "",
+      newPriority: ""
+    }
+  }, 
+  methods: {
+  saveNewTask: function () {
+    this.tasks.push(
+      {name: this.newTask, priority: this.newPriority}
+    );
+    this.newTask = "";
+    this.newPriority = ""
+    }
   }
 }
 </script>
@@ -25,4 +54,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.high {
+  color: red;
+}
+
+.low {
+  color: green;
+}
+
 </style>
